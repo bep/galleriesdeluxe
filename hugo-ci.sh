dbddc1ec381a79f7334cd5a5df29c0d7cfb859c5
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2023-07-04 09:22:46 +0200

    Rework the Dart Sass installation

---
+++ hugo-ci.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export DART_SASS_VERSION=1.63.5
+
+
+curl -LJO https://github.com/sass/dart-sass/releases/download/${DART_SASS_VERSION}/dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz && \
+tar -xf dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz && \
+rm dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz && \
+export PATH=/opt/build/repo/dart-sass:$PATH && \
+sass --version && \
+hugo version && \
+hugo "$@"
